b317121610d6c0d56e4733bc9377c7c25054a8ba
Adrianna Staniek <adrstani@wp.pl>
2022-08-22 20:19:38 +0200

    update 21.08.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+    // style={{
+    //   height: '100vh',
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   fontSize: 40,
+    //   color: '#010101'
+    // }}
     >
-      React homework template
+      <h2>Please leave feedback</h2>
+      <button>Good</button>
+      <button>Neutral</button>
+      <button>Bad</button>
+      <h2>Statistics</h2>
     </div>
   );
 };
